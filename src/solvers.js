e3f6da059b6e3c12d520061133154ecdddee60dc
tivrama <tivrama@yahoo.com>
2016-05-15 19:51:28 -0700

    More progress with help from solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,6 +14,49 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
+  var solution;
+  var board = new Board({n:n});
+
+
+  var subroutine = function(row) {
+    //base - end of row
+    if (row === n) {
+    //increment solution and return
+      solution = _.map(board.rows(), function(row){
+        return row.slice();
+      });
+    }
+
+
+    //go through solutions
+    for (var i = 0; i < n; i++) {
+      //insert piece
+      board.togglePiece(row, i);
+    if (!board.hasAnyRooksConflicts()) {
+      //recuse
+      subroutine(row+1);
+    }
+    //remove piece
+    board.togglePiece(row, i);
+
+    }
+  }
+  subroutine(0)
+
+
+  return solution;
+};
+
+
+
+
+
+
+
+
+
+
+
   // var solution = [];
   // //console.log(n);
   // var rookBoard = new Board({'n':n});   // Creates a new Board
@@ -51,7 +94,7 @@ window.findNRooksSolution = function(n) {
   // console.log('rookRows', rookRows);
   // console.log('solution', rookBoard._currentAttributes);
   // return rookBoard._currentAttributes;
-};
+// };
 
 
 
@@ -63,56 +106,89 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solution = [],
-    board = new Board({'n':n}),
-    currentRow = 0,
-    rowLength = n-1;
-    var testCount = 0
+  var solution = 0;
+  var board = new Board({n:n});
 
 
-    console.log(board.get(0));
-
-    //for a board of 0 sqares, return 0.  For 1 square, return 1
-    if (n === 0) {
-      return 0;
-    }
-    if (n === 1) {
-      return 1;
-    }
-
-  // create a subroutine that goes through the rows (starts with row 1)
-  var subroutine = function(n) {
-    //make base case be when n = 0
-    if (n === 0) {
+  var subroutine = function(row) {
+    //base - end of row
+    if (row === n) {
+    //increment solution and return
+      solution++;
       return;
     }
-    //loop through the current row
-    for (var i = 0; i < rowLength; i++) {
-      //toggle square at current position
-      board.togglePiece(currentRow, i); // This is not currently working??
-      //use helper functions to check if there is a conflict at each column.  If no conflict, recurisvly call subroutine.
-      if (!board.hasAnyRooksConflicts && n !== 1) {
-        n--;
-        currentRow++;
-        subroutine(n);
-      }
+
+
+    //go through solutions
+    for (var i = 0; i < n; i++) {
+      //insert piece
+      board.togglePiece(row, i);
+    if (!board.hasAnyRooksConflicts()) {
+    //recuse
+
+      subroutine(row+1);
+
+    }
+      //remove piece
+    board.togglePiece(row, i);
+
+    }
+  }
+  subroutine(0)
+
+
+  return solution;
+};
+  // var solution = [],
+  //   board = new Board({'n':n}),
+  //   currentRow = 0,
+  //   rowLength = n-1;
+  //   var testCount = 0
+
+
+  //   console.log(board.get(0));
+
+  //   //for a board of 0 sqares, return 0.  For 1 square, return 1
+  //   if (n === 0) {
+  //     return 0;
+  //   }
+  //   if (n === 1) {
+  //     return 1;
+  //   }
+
+  // // create a subroutine that goes through the rows (starts with row 1)
+  // var subroutine = function(n) {
+  //   //make base case be when n = 0
+  //   if (n === 0) {
+  //     return;
+  //   }
+  //   //loop through the current row
+  //   for (var i = 0; i < rowLength; i++) {
+  //     //toggle square at current position
+  //     board.togglePiece(currentRow, i); // This is not currently working??
+  //     //use helper functions to check if there is a conflict at each column.  If no conflict, recurisvly call subroutine.
+  //     if (!board.hasAnyRooksConflicts() && n !== 1) {
+  //       n--;
+  //       currentRow++;
+  //       subroutine(n);
+  //     }
       
 
 
-      if (!board.hasAnyRooksConflicts && n === 1) {
-        n--;
-        currentRow++;
-        solution.concat(board);
-        subroutine(n);
-      }
-    }
-  }
-  subroutine(n);
+  //     if (!board.hasAnyRooksConflicts && n === 1) {
+  //       n--;
+  //       currentRow++;
+  //       solution.concat(board);
+  //       subroutine(n);
+  //     }
+  //   }
+  // }
+  // subroutine(n);
 
-  console.log('solution: ', solution);
-  //console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solution.length;
-};
+  // console.log('solution: ', solution);
+  // //console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // return solution.length;
+// };
 
 
 
@@ -127,8 +203,37 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solution = undefined; //fixme
+  var solution = 0;
+  var board = new Board({n:n});
 
-  //console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+
+  var subroutine = function(row) {
+    //base - end of row
+    if (row === n) {
+    //increment solution and return
+      solution++;
+      return;
+    }
+
+
+    //go through solutions
+    for (var i = 0; i < n; i++) {
+      //insert piece
+      board.togglePiece(row, i);
+    if (!board.hasAnyQueensConflicts()) {
+    //recuse
+
+      subroutine(row+1);
+
+    }
+      //remove piece
+    board.togglePiece(row, i);
+
+    }
+  }
+  subroutine(0)
+
+
+  return solution;
   return solution;
 };
